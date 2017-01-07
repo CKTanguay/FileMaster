eec7824a9702e13535563f33f6b61a2446b8ab26
CKTanguay <CKTanguay@gmail.com>
2017-01-07 17:54:26 -0500

    Conception de base de la fenêtre de l'usager. La combobox se remplit automatiquement selon le type de fichiers présents dans le répertoire sélectionné.

--- FileMaster/MainWindow.Designer.cs
+++ FileMaster/MainWindow.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SelectRepBtn = new System.Windows.Forms.Button();
+            this.ClasserBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.DropDownLabel = new System.Windows.Forms.Label();
+            this.ExtensionComboBox = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // SelectRepBtn
+            // 
+            this.SelectRepBtn.Location = new System.Drawing.Point(14, 12);
+            this.SelectRepBtn.Name = "SelectRepBtn";
+            this.SelectRepBtn.Size = new System.Drawing.Size(200, 85);
+            this.SelectRepBtn.TabIndex = 0;
+            this.SelectRepBtn.Text = "Sélectionner répertoire";
+            this.SelectRepBtn.UseVisualStyleBackColor = true;
+            this.SelectRepBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ClasserBtn
+            // 
+            this.ClasserBtn.Location = new System.Drawing.Point(263, 79);
+            this.ClasserBtn.Name = "ClasserBtn";
+            this.ClasserBtn.Size = new System.Drawing.Size(200, 85);
+            this.ClasserBtn.TabIndex = 1;
+            this.ClasserBtn.Text = "Classer";
+            this.ClasserBtn.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.SelectRepBtn);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 204);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.DropDownLabel);
+            this.panel2.Controls.Add(this.ExtensionComboBox);
+            this.panel2.Location = new System.Drawing.Point(14, 120);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(213, 69);
+            this.panel2.TabIndex = 5;
+            // 
+            // DropDownLabel
+            // 
+            this.DropDownLabel.AutoSize = true;
+            this.DropDownLabel.Location = new System.Drawing.Point(3, 12);
+            this.DropDownLabel.Name = "DropDownLabel";
+            this.DropDownLabel.Size = new System.Drawing.Size(154, 13);
+            this.DropDownLabel.TabIndex = 4;
+            this.DropDownLabel.Text = "Extension des fichiers à classer";
+            // 
+            // ExtensionComboBox
+            // 
+            this.ExtensionComboBox.FormattingEnabled = true;
+            this.ExtensionComboBox.Location = new System.Drawing.Point(6, 28);
+            this.ExtensionComboBox.Name = "ExtensionComboBox";
+            this.ExtensionComboBox.Size = new System.Drawing.Size(194, 21);
+            this.ExtensionComboBox.TabIndex = 3;
+            this.ExtensionComboBox.SelectedIndexChanged += new System.EventHandler(this.button1_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(673, 404);
+            this.ClientSize = new System.Drawing.Size(495, 229);
+            this.Controls.Add(this.ClasserBtn);
+            this.Controls.Add(this.panel1);
             this.Name = "MainWindow";
             this.Text = "FileMaster";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SelectRepBtn;
+        private System.Windows.Forms.Button ClasserBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox ExtensionComboBox;
+        private System.Windows.Forms.Label DropDownLabel;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- FileMaster/MainWindow.cs
+++ FileMaster/MainWindow.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FileMaster
 {
@@ -15,6 +16,33 @@ namespace FileMaster
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (!(string.IsNullOrWhiteSpace(fbd.SelectedPath)))
+            {
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+                string[] fileExtensions=files;
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    files[i] = Path.GetFileName(files[i]);
+                    fileExtensions[i] = Path.GetExtension(files[i]);
+
+                    if (!(ExtensionComboBox.Items.Contains(fileExtensions[i])))
+                    {
+                        ExtensionComboBox.Items.Add(fileExtensions[i]);
+                    }
+                }
+
+                MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                MessageBox.Show("Path: " + fbd.SelectedPath);
+            }
         }
     }
 }
